e95992784cdca4da3caee761bdac6e46c5ef39f3
Jihyun Kim <117818958+jihyunk03@users.noreply.github.com>
2024-01-05 13:14:56 +0000

    Fix: [cpp01/ex04] move to main functions for c++98 flag

--- cpp_module/cpp_module01/ex04/main.cpp
+++ cpp_module/cpp_module01/ex04/main.cpp
@@ -1,38 +1,65 @@
-#include "./Replace.hpp"
+#include <iostream>
+#include <fstream>
+#include <string>
 
-static bool	_check_argument(int ac, char **av);
+static void	_replace(char **arg);
 
 int	main(int ac, char **av)
-{
-	if (_check_argument(ac, av) == false)
-		return 1;
-
-	std::string	outfile = av[1];
-	Replace		r(av, outfile.append(".replace"));
-
-	r.replace();
-
-	return 0;
-}
-
-static bool	_check_argument(int ac, char **av)
 {
 	if (ac != 4)
 	{
 		std::cout << "The number of arguments is not 4" << std::endl;
-		return false;
+		return 1;
 	}
 
-	if (std::strlen(av[1]) == 0 || std::strlen(av[2]) == 0 || std::strlen(av[3]) == 0)
-	{
-		std::cout << "The argument is not valid" << std::endl;
-		return false;
-	}
+	_replace(av);
 
-	return true;
+	return 0;
 }
 
-/*
-$> ./ex04 a.txt '
-' b
-*/
+static void	_replace(char **arg)
+{
+	std::ifstream	ifs;
+	std::ofstream	ofs;
+	std::string		outfile = arg[1];
+	std::string		origin = arg[2];
+	std::string		replace = arg[3];
+	size_t			pos = 0;
+	size_t			origin_len = origin.length();
+	size_t			replace_len = replace.length();
+
+	/* check argument error */
+	if (origin_len == 0)
+	{
+		std::cout << "The argument is not valid" << std::endl;
+		return ;
+	}
+
+	/* check file open */
+	ifs.open(arg[1]);
+	if (ifs.is_open() == false)
+	{
+		std::cout << "Fail to open input-file" << std::endl;
+		return ;
+	}
+	ofs.open(outfile.append(".replace").c_str());
+	if (ofs.is_open() == false)
+	{
+		std::cout << "Fail to open output-file" << std::endl;
+		return ;
+	}
+
+	/* replace */
+	std::string	buffer;
+	std::getline(ifs, buffer, '\0');
+	while (true)
+	{
+		pos = buffer.find(origin, pos);
+		if (pos == std::string::npos)
+			break ;
+		buffer.erase(pos, origin_len);
+		buffer.insert(pos, replace);
+		pos += replace_len;
+	}
+	ofs << buffer;
+}
