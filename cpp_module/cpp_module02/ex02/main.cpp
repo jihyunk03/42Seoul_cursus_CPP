3d15cf4acb46b86ca5cdc19c226b230d4858fc3d
Jihyun Kim <ksk208105@gmail.com>
2024-01-09 11:13:02 +0900

    Chore: [cpp02/ex02] add comments in main.cpp

--- cpp_module/cpp_module02/ex02/main.cpp
+++ cpp_module/cpp_module02/ex02/main.cpp
@@ -6,7 +6,7 @@ int	main(void)
 		Fixed		a;
 		const Fixed	b( Fixed( 5.05f ) * Fixed( 2 ) );
 
-		std::cout << "[incre/decre-ment(pre & post) operator overloading]" << std::endl;
+		std::cout << "==[ incre/decre-ment(pre & post) operator overloading ]==" << std::endl;
 		std::cout << a << std::endl;
 		std::cout << ++a << std::endl;
 		std::cout << a << std::endl;
@@ -18,13 +18,13 @@ int	main(void)
 		std::cout << Fixed::max(a, b) << std::endl;
 	}
 
-	std::cout << "======================================" << std::endl;
+	std::cout << "=========================================================" << std::endl;
 
 	{
 		Fixed	a(5);
-		Fixed	b(3.141592f);
+		Fixed	b(3.14f);
 
-		std::cout << "[comparison operator overloading]" << std::endl;
+		std::cout << "==[ comparison operator overloading ]==" << std::endl;
 		std::cout << "a > b: " << (a > b) << std::endl;
 		std::cout << "a < b: " << (a < b) << std::endl;
 		std::cout << "a >= b: " << (a >= b) << std::endl;
@@ -34,7 +34,7 @@ int	main(void)
 
 		std::cout << std::endl;
 
-		std::cout << "[arithmetic operator overloading]" << std::endl;
+		std::cout << "==[ arithmetic operator overloading ]==" << std::endl;
 		std::cout << "a + b: " << (a + b) << std::endl;
 		std::cout << "a - b: " << (a - b) << std::endl;
 		std::cout << "a * b: " << (a * b) << std::endl;
@@ -42,7 +42,7 @@ int	main(void)
 
 		std::cout << std::endl;
 
-		std::cout << "[min, max function overloading]" << std::endl;
+		std::cout << "==[ min, max function overloading ]==" << std::endl;
 		std::cout << "min(a, b): ";
 		if (Fixed::min(a, b) == a)
 			std::cout << "a !!" << std::endl;
