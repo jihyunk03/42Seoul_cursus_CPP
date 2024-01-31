66637912c9c18ead3eb6bd0fda0502930e9fd169
Jihyun Kim <ksk208105@gmail.com>
2024-02-01 04:10:38 +0900

    Feat: [cpp07/ex02] implementation

---
+++ cpp_module/cpp_module07/ex02/Array.hpp
@@ -0,0 +1,60 @@
+#ifndef ARRAY_HPP
+# define ARRAY_HPP
+
+# include <exception>
+
+template<typename Type>
+class Array
+{
+    private:
+        unsigned int    _arraySize;
+        Type*           _array;
+
+    public:
+        /* OCCF */
+        Array()
+            : _arraySize(0), _array(NULL)
+        {}
+        Array(unsigned int n)
+            : _arraySize(n), _array(new Type[_arraySize])
+        {}
+        Array(const Array& origin)
+            : _arraySize(origin._arraySize), _array(new Type[_arraySize])
+        {
+            for (unsigned int i = 0; i < this->_arraySize; i++)
+                this->_array[i] = origin._array[i];
+        }
+        Array& operator=(const Array& origin)
+        {
+            if (this != &origin)
+            {
+                delete[] _array;
+                this->_arraySize = origin._arraySize;
+                this->_array = new Type[this->_arraySize];
+                for (unsigned int i = 0; i < this->_arraySize; i++)
+                    this->_array[i] = origin._array[i];
+            }
+            return *this;
+        }
+        ~Array()
+        {
+            if (this->_array != NULL)
+                delete[] _array;
+        }
+
+        /* member functions */
+        Type&           operator[](unsigned int index)
+        {
+            if (index < 0 || index >= this->_arraySize)
+                throw std::exception();
+            return this->_array[index];
+        }
+        unsigned int    size() const
+        {
+            return this->_arraySize;
+        }
+};
+
+# include "./Array.tpp"
+
+#endif
---
+++ cpp_module/cpp_module07/ex02/Array.tpp
@@ -0,0 +1,11 @@
+// #include "./Array.hpp"
+
+// template<typename Type>
+// Array::Array()
+// {
+// }
+
+// template<typename Type>
+// Array::~Array()
+// {
+// }
