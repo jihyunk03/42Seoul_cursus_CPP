d726b222327d8bf6e6f410a77aaa1feefc340f43
Jihyun Kim <ksk208105@gmail.com>
2024-02-01 21:36:47 +0900

    Refactor: [cpp07/ex01] main.cpp

--- cpp_module/cpp_module07/ex01/main.cpp
+++ cpp_module/cpp_module07/ex01/main.cpp
@@ -9,13 +9,17 @@ static void _printValue(T tvalue)
 int main(void)
 {
     const size_t    arraySize = 5;
-    int             arrInt[arraySize] = {1, 2, 3, 4, 5};
+    const int             arrInt[arraySize] = {1, 2, 3, 4, 5};
     double          arrDou[arraySize] = {111, 222, 333, 444, 555};
     char            arrCha[arraySize] = {'a', 'b', 'c', 'd', 'e'};
 
+    std::cout << "=============" << std::endl;
     iter(arrInt, arraySize, _printValue);
+    std::cout << "=============" << std::endl;
     iter(arrDou, arraySize, _printValue);
+    std::cout << "=============" << std::endl;
     iter(arrCha, arraySize, _printValue);
+    std::cout << "=============" << std::endl;
 
     return 0;
 }
