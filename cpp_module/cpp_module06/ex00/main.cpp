bfaba840f24ddb573458c6a4cfcb8415a99e3d19
Jihyun Kim <ksk208105@gmail.com>
2024-02-01 18:12:52 +0900

    Refactor: [cpp06/ex00] main.cpp

--- cpp_module/cpp_module06/ex00/main.cpp
+++ cpp_module/cpp_module06/ex00/main.cpp
@@ -2,11 +2,6 @@
 
 int main(int ac, char** av)
 {
-    // (void) ac; (void) av;
-    // float a = -422323233987429834708925723238475923875.1232134213213;
-    // std::cout << std::fixed <<  a << std::endl;
-    // // if (f != f)
-
     if (ac != 2)
     {
         std::cout << "❌ invalid arguments ❌" << std::endl;
