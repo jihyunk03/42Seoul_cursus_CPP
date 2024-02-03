dfd87635e1ac3b751a00ac5977ac84902de091f8
Jihyun Kim <ksk208105@gmail.com>
2024-02-03 17:27:34 +0900

    Fix: [cpp07/ex01] just for evaluation's main ...c...

--- cpp_module/cpp_module07/ex01/iter.hpp
+++ cpp_module/cpp_module07/ex01/iter.hpp
@@ -10,4 +10,12 @@ void iter(Type* array, size_t arrSize, void (*func)(const Type&))
         func(array[i]);
 }
 
+/* for not-const values (just for evaluation-main) */
+template<typename Type>
+void iter(Type* array, size_t arrSize, void (*func)(Type&))
+{
+    for (size_t i = 0; i < arrSize; i++)
+        func(array[i]);
+}
+
 #endif
