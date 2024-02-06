5d88c133e0faf621618bd5f4869aca3ce5e03d78
Jihyun Kim <ksk208105@gmail.com>
2024-02-06 20:57:59 +0900

    Feat: [cpp08/ex00] implementation

--- cpp_module/cpp_module08/ex00/easyfind.hpp
+++ cpp_module/cpp_module08/ex00/easyfind.hpp
@@ -2,13 +2,15 @@
 # define EASYFIND_HPP
 
 # include <iostream>
+# include <algorithm>
 
-template<typename Tarray>
-bool easyfind(Tarray arr, int value)
+template<typename Type>
+typename Type::iterator easyfind(Type& container, int value)
 {
-
+    typename Type::iterator it = std::find(container.begin(), container.end(), value);
+    if (it == container.end())
+        std::cerr << "'" << value << "' is not in this container" << std::endl;
+    return it;
 }
-
-# include "./easyfind.tpp"
 
 #endif
--- cpp_module/cpp_module08/ex00/main.cpp
+++ cpp_module/cpp_module08/ex00/main.cpp
@@ -1,6 +1,48 @@
 #include "./easyfind.hpp"
+#include <vector>
+#include <deque>
 
 int main(void)
 {
+    {
+        std::vector<int>            vecInt;
+        std::vector<int>::iterator  itInt;
+
+        vecInt.push_back(4);
+        vecInt.push_back(16);
+        vecInt.push_back(54);
+        vecInt.push_back(64);
+        vecInt.push_back(256);
+
+        itInt = easyfind(vecInt, 16);
+        if (itInt != vecInt.end())
+            std::cout << "find value: " << *itInt << std::endl;
+
+        itInt = easyfind(vecInt, 77);
+        if (itInt != vecInt.end())
+            std::cout << "find value: " << *itInt << std::endl;
+    }
+
+    std::cout << "=====================================" << std::endl;
+
+    {
+        std::deque<int>             vecDq;
+        std::deque<int>::iterator   itInt;
+
+        vecDq.push_back(4);
+        vecDq.push_back(16);
+        vecDq.push_back(54);
+        vecDq.push_back(64);
+        vecDq.push_back(256);
+
+        itInt = easyfind(vecDq, 16);
+        if (itInt != vecDq.end())
+            std::cout << "find value: " << *itInt << std::endl;
+
+        itInt = easyfind(vecDq, 77);
+        if (itInt != vecDq.end())
+            std::cout << "find value: " << *itInt << std::endl;
+    }
+
     return 0;
 }
