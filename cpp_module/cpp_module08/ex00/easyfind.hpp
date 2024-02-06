753c980ddc1eda37ce9525b55b2c0753be71293b
Jihyun Kim <ksk208105@gmail.com>
2024-02-06 15:43:15 +0900

    Add: [cpp08/ex00] default files

---
+++ cpp_module/cpp_module08/ex00/easyfind.hpp
@@ -0,0 +1,14 @@
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+# include <iostream>
+
+template<typename Tarray>
+bool easyfind(Tarray arr, int value)
+{
+
+}
+
+# include "./easyfind.tpp"
+
+#endif
