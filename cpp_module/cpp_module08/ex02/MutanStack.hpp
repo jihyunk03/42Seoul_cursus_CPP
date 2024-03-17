6135b37e28281143e14f958a433cca9ac8407ead
Jihyun Kim <ksk208105@gmail.com>
2024-03-17 19:30:49 +0900

    Add: [cpp08/ex02] default files

---
+++ cpp_module/cpp_module08/ex02/MutanStack.hpp
@@ -0,0 +1,20 @@
+#ifndef MUTANSTACK_HPP
+# define MUTANSTACK_HPP
+
+# include <iostream>
+# include <algorithm>
+
+# include <stack>   // ????
+
+template<typename T>
+class MutanStack
+{
+    private:
+
+    public:
+        /* OCCF */
+        MutanStack();
+        ~MutanStack();
+};
+
+#endif
