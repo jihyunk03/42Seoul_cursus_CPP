f7e18ded09c87dbe4d75d5d94db25642ec83c984
Jihyun Kim <ksk208105@gmail.com>
2024-03-27 18:39:21 +0900

    Feat: [cpp09/ex00] exception handling

--- cpp_module/cpp_module09/ex00/main.cpp
+++ cpp_module/cpp_module09/ex00/main.cpp
@@ -2,15 +2,17 @@
 
 int main(int ac, char** av)
 {
-    try
-    {
-        BitcoinExchange btc();
-
-        // btc.exec(av[1]);
+    if (ac != 2) {
+        std::cout << "Error: only one database to input" << std::endl;
+        return 1;
     }
-    catch(const std::exception& e)
-    {
+
+    try {
+        BitcoinExchange btc;
+        btc.calculate_bitcoin(av[1]);
+    } catch(const std::exception& e) {
         std::cerr << e.what() << std::endl;
+        return 1;
     }
 
     return 0;
