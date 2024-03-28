70f03efb1ded3f3c8a1cb4f8f349c86372839523
Jihyun Kim <ksk208105@gmail.com>
2024-03-28 21:21:09 +0900

    Feat: [cpp09/ex01] RPN class implementation

--- cpp_module/cpp_module09/ex01/RPN.cpp
+++ cpp_module/cpp_module09/ex01/RPN.cpp
@@ -1,9 +1,84 @@
 #include "./RPN.hpp"
 
-RPN::RPN()
+/* OCCF: constructor */
+RPN::RPN() {}
+
+/* OCCF: destructor */
+RPN::~RPN() {}
+
+/* member function: public */
+void RPN::calculate(const std::string& args)
 {
+    std::istringstream  iss(args);
+    std::string         token;
+
+    this->_clearStack();
+    try {
+        while (std::getline(iss, token, ' ')) {
+            if (token.size() == 0)                  // row of blanks
+                continue;
+            int type = this->_checkType(token);     // operator or valid number
+            if (type == OPERATOR)
+                this->_calculateInStack(token);
+            else
+                this->_stack.push(type);
+        }
+        if (this->_stack.size() != 1)
+            throw std::logic_error("Error: not enough operators for calculating");
+        std::cout << "[result]: " << this->_stack.top() << std::endl;
+    } catch(const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+    }
 }
 
-RPN::~RPN()
+/* member function: private */
+int RPN::_checkType(const std::string& token)
 {
+    char*   pos;
+    int     res;
+
+    if (token.size() != 1)
+        throw std::logic_error("Error: invalid token(only 0 ~ 9) => " + token);
+    if (token == "+" || token == "-" || token == "*" || token == "/")
+        return OPERATOR;
+    res = strtod(token.c_str(), &pos);
+    std::string remaining(pos);
+    if (remaining.empty() == false || res < 0 || res > 9)
+        throw std::logic_error("Error: not a number => " + token);
+    return res;
+}
+
+void RPN::_calculateInStack(const std::string& op)
+{
+    int firstNum;
+    int secondNum;
+
+    if (this->_stack.size() < 2)
+        throw std::logic_error("Error: not enough numbers for calculating");
+    secondNum = this->_stack.top();
+    this->_stack.pop();
+    firstNum = this->_stack.top();
+    this->_stack.pop();
+    this->_stack.push(this->_calculateNumbers(firstNum, secondNum, op));
+}
+
+int RPN::_calculateNumbers(int fn, int sn, const std::string& op)
+{
+    if (op == "+")
+        return fn + sn;
+    else if (op == "-")
+        return fn - sn;
+    else if (op == "*")
+        return fn * sn;
+    else {
+        if (sn == 0)
+            throw std::logic_error("Error: zero denominator in division calculating");
+        return fn / sn;
+    }
+}
+
+void RPN::_clearStack(void)
+{
+    while (this->_stack.empty() == false)
+        this->_stack.pop();
 }
--- cpp_module/cpp_module09/ex01/RPN.hpp
+++ cpp_module/cpp_module09/ex01/RPN.hpp
@@ -2,19 +2,39 @@
 # define RPN_HPP
 
 # include <iostream>
+# include <sstream>
 # include <algorithm>
 # include <stack>
+
+# define OPERATOR -1
+
+/**
+ * stack에 관련된 연산 시, empty로 확인하는 경우 거의 없어서.. (abort 주의하기)
+*/
 
 class RPN
 {
     private:
+        /* OCCF */
+        RPN(const RPN& origin);
+        RPN& operator=(const RPN& origin);
+
+        /* member variable */
+        std::stack<int> _stack;
+
+        /* member function */
+        int     _checkType(const std::string& token);
+        void    _calculateInStack(const std::string& op);
+        int     _calculateNumbers(int fn, int sn, const std::string& op);
+        void    _clearStack(void);
 
     public:
         /* OCCF */
         RPN();
-        RPN(const RPN& origin);
-        RPN& operator=(const RPN& origin);
         ~RPN();
+
+        /* member function */
+        void    calculate(const std::string& args);
 };
 
 #endif
