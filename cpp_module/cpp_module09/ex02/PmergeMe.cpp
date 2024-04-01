230966321c08fca84e84b141ceedc0115c0c425c
Jihyun Kim <ksk208105@gmail.com>
2024-04-01 22:02:39 +0900

    Feat: [cpp09/ex02] mergesort & jacobsthal arr implementation

--- cpp_module/cpp_module09/ex02/PmergeMe.cpp
+++ cpp_module/cpp_module09/ex02/PmergeMe.cpp
@@ -1,16 +1,102 @@
 #include "./PmergeMe.hpp"
 
 /* OCCF: constructor */
-PmergeMe::PmergeMe()
+PmergeMe::PmergeMe(int ac, char** av)
 {
+    if (ac < 3)
+        throw std::logic_error("Error: arguments are more than 3");
+    this->_vecArgs.clear();
+    // this->_dq.clear();
+    this->_checkNPush(ac, av);
+    this->_calculateJacobsthalArr(ac / 2);
 }
 
 /* OCCF: destructor */
 PmergeMe::~PmergeMe()
-{
-}
+{}
 
 /* member function: public */
+void PmergeMe::fordJohnsonAlVec(void)
+{
+    // this->_mainChain.clear();
+    // this->_pendingChain.clear();
+    this->_makePairNSortVec();
+    this->_mergeSortRecursiveVec(0, this->_vecPair.size() - 1);
+}
+
 
 
 /* member function: private */
+void PmergeMe::_checkNPush(int count, char** args)
+{
+    for (int i = 1; i < count; i++) {
+        char* pos;
+        long num = strtol(args[i], &pos, 10);
+        if (*pos != '\0')               // string에 넣어서 empty 검사하는 형식 아니어도 됨 ㅇㅇ
+            throw std::logic_error("Error: arguments are must be integer");
+        if (std::find(this->_vecArgs.begin(), this->_vecArgs.end(), num) != this->_vecArgs.end())
+            throw std::logic_error("Error: all numbers must be different from each other");
+        if (num <= 0)
+            throw std::logic_error("Error: all numbers must be positive integer");
+        this->_vecArgs.push_back(num);
+        // this->_dq.push_back(num);
+    }
+}
+
+void PmergeMe::_calculateJacobsthalArr(int count)
+{
+    if (this->_jacobNum.empty() == false)
+        this->_jacobNum.clear();
+    this->_jacobNum.push_back(1);
+    this->_jacobNum.push_back(3);
+
+    int curr = 3, now = 2;
+    while (curr <= count) {
+        curr = this->_jacobNum[now - 1] + 2 * this->_jacobNum[now - 2];
+        this->_jacobNum.push_back(curr);
+        ++now;
+    }
+}
+
+void PmergeMe::_makePairNSortVec(void)
+{
+    // if (this->_vecPair.empty() == false)
+    //     this->_vecPair.clear();
+    for (int i = 0; i < this->_vecArgs.size(); ++(++i)) {
+        if (i == this->_vecArgs.size() - 1)
+            break;
+        if (this->_vecArgs[i] > this->_vecArgs[i + 1])
+            this->_vecPair.push_back(std::make_pair(this->_vecArgs[i], this->_vecArgs[i + 1]));
+        else
+            this->_vecPair.push_back(std::make_pair(this->_vecArgs[i + 1], this->_vecArgs[i]));
+    }
+}
+
+void PmergeMe::_mergeSortRecursiveVec(int left, int right)
+{
+    if (left >= right)
+        return;
+
+    int mid = (left + right) / 2;
+    this->_mergeSortRecursiveVec(left, mid);
+    this->_mergeSortRecursiveVec(mid + 1, right);
+    std::vector<std::pair<long, long>> temp(this->_vecPair);
+    int l = left, r = mid + 1, now = left;
+    while (l <= mid && r <= right) {
+        if (this->_vecPair[l].first <= this->_vecPair[r].first)
+            temp[now++] = this->_vecPair[l++];
+        else
+            temp[now++] = this->_vecPair[r++];
+    }
+    int remain = l;
+    if (l == mid)
+        remain = r;
+    while (now <= right)
+        temp[now++] = this->_vecPair[remain++];
+    this->_vecPair = temp;
+}
+
+void PmergeMe::_binarySearchInsertionVec(void)
+{
+    // 반드시 홀수인 경우에 대해 pendingChain에 마지막 원소를 추가해 주어야 함
+}
--- cpp_module/cpp_module09/ex02/PmergeMe.hpp
+++ cpp_module/cpp_module09/ex02/PmergeMe.hpp
@@ -9,13 +9,41 @@ class PmergeMe
 {
     private:
         /* OCCF */
+        PmergeMe();
         PmergeMe(const PmergeMe& origin);
         PmergeMe& operator=(const PmergeMe& origin);
 
+        /* member variable */
+        std::vector<long>                   _vecSorted;     // 최종 sort 결과물
+
+        std::vector<long>                   _vecArgs;       // input argumets(origin)
+        std::vector<std::pair<long, long>>  _vecPair;       // 역으로 정렬된 짝이 이뤄진 벡터로, mainChain과 pendingChain을 결정하기 위한 값(계속 갱신될 값)
+
+        std::vector<long>                   _mainChain;     // 맴버 변수로는 적합하진 않은것 같은데 이들에 대해 다시 고민해보기
+        std::vector<long>                   _pendingChain;  // clear 진행하고 하던가 아니면 아예 없애던가 하자
+
+        std::vector<int>                    _jacobNum;      // dq까지는 만들 이유 없음
+
+        // std::deque<int>     _dq;
+
+        /* member function */
+        void    _checkNPush(int count, char** args);
+        void    _calculateJacobsthalArr(int count);       // 생성자 단계에서 진행
+
+        void    _makePairNSortVec(void);
+        void    _mergeSortRecursiveVec(int left, int right);
+        void    _binarySearchInsertionVec(void);
+
+        // void    _fordJohnsonAlDq(void);
+
     public:
         /* OCCF */
-        PmergeMe();
+        PmergeMe(int ac, char** av);
         ~PmergeMe();
+
+        /* member function */
+        void    fordJohnsonAlVec(void);
+        // print ?
 };
 
 #endif
