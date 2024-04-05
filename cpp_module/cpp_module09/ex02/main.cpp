5cf479184cd72c03471d31334be4a825265855bd
Jihyun Kim <ksk208105@gmail.com>
2024-04-05 23:29:20 +0900

    Feat: [cpp09/ex02] for execute Pmerge-program

--- cpp_module/cpp_module09/ex02/main.cpp
+++ cpp_module/cpp_module09/ex02/main.cpp
@@ -3,11 +3,15 @@
 int main(int ac, char** av)
 {
     try {
-        // PmergeMe    fordJohnson(av);
+        PmergeMe    fordJohnson(ac, av);
 
         // clock -> vector
         // clock -> deque
         // print compare results
+        fordJohnson.runFordJohnson();
+        for (int i = 0; i < ac - 1; ++i) {
+            std::cout << fordJohnson.getSortedVec()[i] << std::endl;
+        }
     } catch(const std::exception& e) {
         std::cerr << e.what() << std::endl;
     }
